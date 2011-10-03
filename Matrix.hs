d9573afa230b56590f682d03470dde87e01cd4bb
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2011-10-02 23:56:36 -0400

    mul function implemented

--- Matrix.hs
+++ Matrix.hs
@@ -1,1 +1,20 @@
 module Matrix
+where
+
+type Matrix = [[Integer]]
+
+add :: Matrix -> Matrix -> Matrix
+add lm rm = zipWith (zipWith (+)) lm rm
+
+sub :: Matrix -> Matrix -> Matrix
+sub lm rm = zipWith (zipWith (-)) lm rm
+
+transpose :: Matrix -> Matrix
+transpose (m:[]) = [ [e] | e <- m ]
+transpose (m:ms) = zipWith (++) (transpose (m:[])) (transpose ms)
+
+cross :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+cross f l r = [ map (f le) r | le <- l ]
+
+mul :: Matrix -> Matrix -> Matrix
+mul l r = cross (\x y -> sum $ zipWith (*) x y) l (transpose r)
